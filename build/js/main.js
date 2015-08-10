c3d92a5b6a32b9f3e77f83a37b93c47c6e331db2
Валерий <kaktuz34@gmail.com>
2015-08-10 19:01:36 +0300

    upd

--- build/js/main.js
+++ build/js/main.js
@@ -101,4 +101,37 @@ $(document).ready( function() {
 	}
 	navOpen();
 
+	// spiner
+	function spiner() {
+		var number = $('.js-spiner');
+		number.each(function(){
+			var max_number = +($(this).attr('data-max-number'));
+			var input = $(this).find('input');
+			var plus = $(this).find('.js-plus');
+			var minus = $(this).find('.js-minus');
+			plus.on('click', function(){
+				var val = +(input.val());
+				if (val >= max_number) {
+					return false;
+				}
+				else {
+					val += 1;
+					input.val(val);
+				}
+			});
+			minus.on('click', function(){
+				var val = +(input.val());
+				if (val > 1) {
+					val -= 1;
+					input.val(val);
+				}
+				else {
+					input.val('1');
+					return false;
+				}
+			});
+		});
+	}
+	spiner();
+
 });
